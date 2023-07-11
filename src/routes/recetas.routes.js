4a5431e05e8bf436d255c73e2ec3040e1c0f0c62
Silvio Ruiz <silviojrck@gmail.com>
2023-07-11 20:52:15 -0300

    Agregamos endpoint para obtener una sola receta

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { crearReceta, obtenerRecetas } from "../controllers/recetas.controller";
+import { crearReceta, obtenerReceta, obtenerRecetas } from "../controllers/recetas.controller";
 
 const router = Router();
 
 router.route('/recetas').get(obtenerRecetas).post(crearReceta);
+
+router.route('/recetas/:id').get(obtenerReceta);
 
 export default router;
